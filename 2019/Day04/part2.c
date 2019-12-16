679be353bd9ff1b45fdc7dcba5e7c6fa12ceef75
Michael Herman <michael@thehermans.org>
2019-12-16 17:13:55 -0600

    working on day 4 part 2

--- 2019/Day04/part2.c
+++ 2019/Day04/part2.c
@@ -6,9 +6,11 @@
 int main()
 {
 
-    int startInt = 367479;
+    // int startInt = 367479;
+    int startInt = 399000;
     int endInt = 893698;
     int validCount = 0;
+    int newInt = startInt;
 
     int counter[6];
 
@@ -18,23 +20,38 @@ int main()
         startInt = startInt / 10;
     };
 
-    while (true)
+    while (newInt <= endInt)
     {
         for (int pos = 5; pos >= 0; pos--)
         {
             int currPos = counter[pos];
             int nextPos = counter[pos - 1];
 
-            if (currPos >= nextPos)
+            if (pos == 0)
             {
-                
+                counter[5] = (counter[5] + 1) % 10;
+                for (int i = 5; i >= 0; i--)
+                {
+                    if (counter[i] == 0)
+                    {
+                        counter[i - 1] = (counter[i - 1] + 1) % 10;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            else if (currPos >= nextPos)
+            {
+
                 continue;
             }
-            else if (currPos == 0)
-            {
-                counter[pos - 1]++;
-                continue;
-            }
+            // else if (currPos == 0)
+            // {
+            //     counter[pos - 1] = counter[pos - 1] % 10;
+            //     continue;
+            // }
             else if (currPos < nextPos)
             {
 
@@ -44,34 +61,29 @@ int main()
                     pos++;
                 }
             }
-            else if ( pos == 0 ) {
-                counter[5]++;
-            }
 
-
-            int newInt = 0;
+            newInt = 0;
             for (int z = 0; z < 6; z++)
             {
+
                 newInt = 10 * newInt + counter[z];
             }
-
-            printf("int is %d\n", newInt);
-
-            if (newInt >= endInt)
+            if (newInt == endInt)
             {
                 break;
-            } else {
-                pos = 5;
-                counter[5]++;
             }
+            printf("int is %d\n", newInt);
         }
-        
-        break;
-    }
 
-    // for (int i = 0; i < 6; i++){
-    //     printf ("%d %d\n", i, counter[i]);
-    // }
+        // if (newInt >= endInt)
+        // {
+        //     break;
+        // }
+    }
 
     return 0;
 }
+
+// for (int i = 0; i < 6; i++){
+//     printf ("%d %d\n", i, counter[i]);
+// }

ed8b6625d02d96a24788e64bce86efe9fa6b41b4
Michael Herman <michael@thehermans.org>
2020-12-03 20:56:31 -0600

    working on part1

--- 2020/Day03/part1.c
+++ 2020/Day03/part1.c
@@ -15,16 +15,20 @@ int main(void){
     int **forest = malloc(sizeof(int*));
     char chunk[256];
     int counter = 0;
+    
 
     while (fgets(chunk, sizeof(chunk), fp) != NULL) {
         int rowLen = strlen(chunk);
-        if (strcmp(&chunk[rowLen],"\n")) {
-            rowLen = rowLen -1;
-        }
+        chunk[rowLen -1] = '\0';
         forest[counter] = malloc(sizeof(int)*rowLen);
+        char item;
+
+        // printf("first character is %s\n",&chunk[0]);
+        // printf("strcmp test is %d\n",strcmp(&chunk[0],"."));
 
         for (int i = 0; i<rowLen;i++) {
-            if (strcmp(&chunk[i],".")) {
+            item = chunk[i];
+            if (strcmp(&item,".") == 0) {
                 forest[counter][i] = 0;
             } else {
                 forest[counter][i] = 1;
@@ -39,8 +43,9 @@ int main(void){
 
     for (int i = 0;i<counter;i++) {
         for (int j = 0;j<32;j++) {
-            printf("%c", forest[i][j]);
+            printf("%d", forest[i][j]);
         }
+        printf("\n");
     }
     return 0;
 

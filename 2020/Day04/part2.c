4f7722f4c74539550ee30c7e992b000cfbde5c8a
Michael Herman <michael@thehermans.org>
2020-12-11 13:31:03 -0600

    updates part2 - maybe done

--- 2020/Day04/part2.c
+++ 2020/Day04/part2.c
@@ -7,8 +7,8 @@
 
 typedef enum { False, True } boolean;
 
-void getField(char *chunk,int *validFieldCount);
-boolean checkDates (char *field, int start, int end) 
+void getField(char *chunk,int *validFieldCount, boolean *cidExists);
+boolean checkDates (char *field, int start, int end);
 
 
 int main(void){
@@ -56,7 +56,7 @@ int main(void){
                 char *token = strtok_r(chunk, " ",&savePtr);
 
                 while ( token != NULL) {
-                    getField(token, &validFieldCount);            
+                    getField(token, &validFieldCount, &cidExists);
                     token = strtok_r(NULL, " ",&savePtr);
                 }
             }
@@ -67,7 +67,7 @@ int main(void){
 
 }
 
-void getField(char *field, int *validFieldCount) {
+void getField(char *field, int *validFieldCount, boolean *cidExists) {
     char *fieldValue;
     char *fieldToken = strtok_r(field,":",&fieldValue);
 //    while ( fieldToken != NULL) {
@@ -77,7 +77,7 @@ void getField(char *field, int *validFieldCount) {
             int startYear = 1920;
             int endYear = 2002;
             if (checkDates(fieldToken, startYear, endYear) == True) {
-                validFieldCount++;
+                *validFieldCount = *validFieldCount + 1;
             }
             return;
         }
@@ -85,7 +85,7 @@ void getField(char *field, int *validFieldCount) {
             int startYear = 2010;
             int endYear = 2020;
             if (checkDates(fieldToken, startYear, endYear) == True) {
-                validFieldCount++;
+                *validFieldCount = *validFieldCount + 1;
             }
             return;
         }
@@ -93,38 +93,74 @@ void getField(char *field, int *validFieldCount) {
             int startYear = 2020;
             int endYear = 2030;
             if (checkDates(fieldToken, startYear, endYear) == True) {
-                validFieldCount++;
+                *validFieldCount = *validFieldCount + 1;
             }
             return;
         }
         if (strcmp(fieldKey,"hgt") == 0) {
-            char units[2] = fieldToken[strlen(fieldToken) -2];
+            char units[3] = {0};
+            char heightString[20];
+            char *eptr;
+            unsigned long heightLength = strlen(fieldToken) -2;
+            strncpy(heightString, fieldToken, heightLength);
+            heightString[heightLength] = '\0';
+            long height = strtol(heightString, &eptr, 10);
+            strncpy(units,&fieldToken[strlen(fieldToken) - 2],2);
+            if (strcmp(units,"cm") == 0){
+                if (height >= 150 && height <= 193) {
+                    *validFieldCount = *validFieldCount + 1;
+                }
+            } else if (strcmp(units,"in") == 0) {
+                if (height >= 59 && height <= 76) {
+                    *validFieldCount = *validFieldCount + 1;
+                }
+            }
+            return;
 
         }
         if (strcmp(fieldKey,"hcl") == 0) {
-            
+            if (fieldToken[0] == '#' && strlen(fieldToken) == 7) {
+                for (int i = 1; i < 7; i++) {
+                    if ((fieldToken[i] < '0' && fieldToken[i] > '9') || (fieldToken[i] < 'a' && fieldToken[i] > 'f') ) {
+                        return;
+                    }
+                }
+                *validFieldCount = *validFieldCount + 1;
+            }
             return;
         }
         if (strcmp(fieldKey,"ecl") == 0) {
-
+            char *eyeColors[7] = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+            for (int i = 0; i < 7; i++) {
+                if (strcmp(fieldToken,eyeColors[i]) == 0) {
+                    *validFieldCount = *validFieldCount + 1;
+                    break;;
+                }
+            }
             return;
         }
         if (strcmp(fieldKey,"pid") == 0) {
-            
+            if (strlen(fieldToken) == 9) {
+                for (int i = 0; i < 9; i++)
+                    if(fieldToken[i] < '0' && fieldToken[i] > '9' ) {
+                        return;
+                    }
+                    *validFieldCount = *validFieldCount + 1;                   
+            }
             return;
         }
         if (strcmp(fieldKey,"cid") == 0) {
-
+            *validFieldCount = *validFieldCount + 1;        
+            *cidExists = True;
             return;
-//    }
-    
+        }
 }
 
 boolean checkDates (char *field, int start, int end) {
-    long startYear = strotol(start, 10);
-    long endYear = strtol(end, 10);
-    long fieldValue = strtol(field, 10);
-    if (fieldValue >= startYear && fieldValue <= endYear) {
+    char *tmpValue;
+    long fieldValue = strtol(field, &tmpValue, 10);
+    if (fieldValue >= start && fieldValue <= end) {
         return True;
     }
+    return False;
 }

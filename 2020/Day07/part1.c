cb2324de9a40932e4ee654f01babb547e7af40bd
Michael Herman <michael@thehermans.org>
2021-02-03 16:12:11 -0600

    updates day 7

--- 2020/Day07/part1.c
+++ 2020/Day07/part1.c
@@ -5,10 +5,11 @@
 
 
 
-#define myBag "shiny gold" 
+#define myBag "shiny gold"
 
 void deleteItem (char **array, int *position, int *counter);
 void findBags(char **chunks, int counter, char **bags, int *bagCounter);
+void makeBagList (char *chunk, char **chunks, int *counter);
 
 
 int main() {
@@ -29,36 +30,33 @@ int main() {
     char **chunks;
     int counter = 0;
 
-    chunks = calloc(strlen(bag), sizeof(char) * sizeof(bag));
+    chunks = malloc(sizeof(char));
 
     while (fgets(chunk, sizeof(chunk), fp) != NULL) {
-    //     char *contains = strstr(chunk,"contain");
-
-    //     for (int i = 0; i < bagCounter; i++) {
-    //         if(strstr(contains, bags[i])) {
-    //             printf("found your bag in %s", chunks[i]);
-    //             sscanf(chunks[i], "%s %s",bag,color);
-    //             strcat(bag, " ");
-    //             strcat(bag, color);
-    //             bags[bagCounter] = malloc(sizeof(bag));
-    //             strcpy(bags[bagCounter], bag);
-    //             bagCounter++;
-    //             bags = realloc(bags, sizeof(bag) * (bagCounter + 1));
-    //         }
-    //     }
-    // }
-        chunks[counter] = calloc(strlen(chunk),sizeof(char));
-        strcpy(chunks[counter],chunk);
-
-        chunks = realloc(chunks,sizeof(char) * (counter + 1));
-        printf("file line %d size of chunk %lu size of chunk %lu\n", counter,strlen(chunk),sizeof(chunks[counter]));
-        counter++;
+        makeBagList(chunk, chunks, &counter);
 
     }
 
     for (int i = 0; i < counter; i++) {
         printf("%d %s\n",i, chunks[i]);
+        free(chunks[i]);
     }
 
+    free (chunks);
+
 return 0;
+}
+
+void makeBagList (char *chunk, char **chunks, int *counter){
+        if (chunk[strlen(chunk) - 1] == '\n') {
+            chunk[strlen(chunk) - 1] = '\0';
+        }
+        *chunks = realloc(chunks, (*counter + 1) * sizeof(char));
+        chunks[*counter] = malloc(strlen(chunk) * sizeof(char));
+        strcpy(chunks[*counter],chunk);
+
+ 
+        // printf("file line %d size of chunk %lu size of chunk %lu\n", *counter,strlen(chunk),sizeof(chunks[*counter]));
+        *counter = *counter + 1;
+        chunks = realloc(chunks,sizeof(*chunks) * (*counter + 1));
 }
